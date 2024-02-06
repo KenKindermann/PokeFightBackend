9fc34b63c4781ba5c4329efb8929636eb3701c26
Tonio <tonio.suessdorf@gmail.com>
2024-02-06 16:51:09 +0100

    Change port to use render env variable and server to use cors

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import pokemonRouter from "./routes/pokemonRoutes.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use("/pokemon", pokemonRouter);
 
-// app.use(cors());
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
