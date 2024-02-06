06cdb1c4fdb426f7ca5a7844492d1b52483e7390
Tonio <tonio.suessdorf@gmail.com>
2024-02-06 15:56:41 +0100

    Add route to fetch pokemon fro pokeAPI; switch from commonJS to module

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+// import cors from "cors";
+import pokemonRouter from "./routes/pokemonRoutes.js";
+
 const app = express();
 
-app.use(cors());
+app.use("/pokemon", pokemonRouter);
+
+// app.use(cors());
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
