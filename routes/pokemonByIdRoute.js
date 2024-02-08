263f2e6e722046688f6dee64399a653be633ce22
Oskar <oskar.radetzki.dev@googlemail.com>
2024-02-08 14:42:35 +0100

    break

--- routes/pokemonByIdRoute.js
+++ routes/pokemonByIdRoute.js
@@ -5,9 +5,10 @@ const pokemonByIdRouter = express.Router();
 
 pokemonByIdRouter.get("/:id", async (req, res) => {
     const {id} = req.params;
+    console.log({id})
 
-    try {
-      const pokemons = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=150&limit=150/");
+    try { 
+      const pokemons = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
       res.json(pokemons.results.id[0]);
       console.log(pokemons.results.id)
     } catch (error) {
