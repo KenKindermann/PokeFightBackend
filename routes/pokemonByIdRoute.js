22ca4621103bc51be18056729a4a7fc8e6b1d431
Oskar <oskar.radetzki.dev@googlemail.com>
2024-02-08 16:34:06 +0100

    Get pokemons by id

--- routes/pokemonByIdRoute.js
+++ routes/pokemonByIdRoute.js
@@ -4,12 +4,12 @@ import axios from "axios";
 const pokemonByIdRouter = express.Router();
 
 pokemonByIdRouter.get("/:id", async (req, res) => {
-    const id = req.params;
+    const id = req.params.id;
 
     try { 
       const pokemons = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-      res.json(pokemons.results.id[0]);
-      console.log(pokemons.results.id)
+      res.json(pokemons.data);
+      console.log(pokemons.data)
     } catch (error) {
       console.error("Failed to fetch Pokémon", error);
       res.status(500).send("Error fetching Pokémon data");
