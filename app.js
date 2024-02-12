b077492ceb022e7612bb2fc181e995224fd26e59
Oskar <oskar.radetzki.dev@googlemail.com>
2024-02-12 16:22:58 +0100

    Leaderboard function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import pokemonRouter from "./routes/pokemonRoutes.js";
-import leaderboardRouter from "./routes/leaderboardRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use("/pokemon", pokemonRouter)
 app.use("/:id", pokemonRouter);
-app.use("/leaderboard", leaderboardRouter)
+app.use('/updateLeaderboard', pokemonRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   fetchAllPokemonDetails,
   pokemonById,
+  updateLeaderboard
 } from "../controllers/pokemonController.js";
 
 const pokemonRouter = express.Router();
 
 pokemonRouter.get("/:id", pokemonById);
 pokemonRouter.get("/", fetchAllPokemonDetails);
+pokemonRouter.post('/updateLeaderboard', updateLeaderboard);
 
 export default pokemonRouter;
