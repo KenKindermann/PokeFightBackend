8d6d0910ed191d9a4ce78049e195aa4df636bd1d
Tonio <tonio.suessdorf@gmail.com>
2024-02-12 17:11:49 +0100

    Fix deployment error by reverting app.js renaming

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "./db/server.js";
+import pokemonRouter from "./routes/pokemonRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/pokemon", pokemonRouter);
+app.use("/:id", pokemonRouter);
+app.use("/updateLeaderboard", pokemonRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
