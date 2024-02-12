e649c27e9927cd1792540df42fe39d2f8a283d34
Oskar <oskar.radetzki.dev@googlemail.com>
2024-02-12 13:36:54 +0100

    leaderboardPoints

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import pokemonRouter from "./routes/pokemonRoutes.js";
+import leaderboardRouter from "./routes/leaderboardRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +10,7 @@ app.use(express.json())
 app.use(cors());
 app.use("/pokemon", pokemonRouter)
 app.use("/:id", pokemonRouter);
-app.use("/leaderboard", leaderboardRoute)
+app.use("/leaderboard", leaderboardRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
