3206dea01c95e25842ef3210cc2ef6c2419408a8
Oskar <oskar.radetzki.dev@googlemail.com>
2024-02-12 13:43:01 +0100

    fix

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import leaderboardRouter from "./routes/leaderboardRoute.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
 app.use(cors());
 app.use("/pokemon", pokemonRouter)
 app.use("/:id", pokemonRouter);
