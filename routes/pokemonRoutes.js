3751ef66bb4fdbe09582c065f0583a7d89a48143
Tonio <tonio.suessdorf@gmail.com>
2024-02-14 16:32:19 +0100

    Add dbInit script; change update pokemon route to be put request

--- routes/pokemonRoutes.js
+++ routes/pokemonRoutes.js
@@ -2,15 +2,15 @@ import express from "express";
 import {
   fetchAllPokemonDetails,
   pokemonById,
-  updatePokemonScore,
   getLeaderboard,
 } from "../controllers/pokemonController.js";
+import { updatePokemonScore } from "../controllers/leaderboardController.js";
 
 const pokemonRouter = express.Router();
 
 pokemonRouter.get("/:id", pokemonById);
 pokemonRouter.get("/", fetchAllPokemonDetails);
-pokemonRouter.post("/game/update-score/", updatePokemonScore);
+pokemonRouter.put("/game/update-score/", updatePokemonScore);
 pokemonRouter.get("/game/leaderboard", getLeaderboard);
 
 export default pokemonRouter;
